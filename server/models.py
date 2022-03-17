bca421c4b96e3f241aec2556b9bbfbfa0b1ac8b0
574515 <574515c0d1ng@gmail.com>
2022-03-17 11:27:25 +0100

    Added database models

--- server/models.py
+++ server/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+	player1                     = models.CharField(max_length=30)
+	player2                     = models.CharField(max_length=30)
+	winner                      = models.CharField(max_length=30)
+
+	def __str__(self):
+		return f'{self.player1} vs {self.player2} -> {self.winner}'
+
+
+class Move(models.Model):
+	move_number                 = models.IntegerField()
+	affected_field              = models.IntegerField()
+	game						= models.ForeignKey(Game, related_name='moves', on_delete=models.CASCADE, blank=False, null=False)
+
+	def __str__(self):
+		return str(f'Move: {self.move_number} on field {self.affected_filed}')
