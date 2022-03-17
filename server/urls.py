71162c4caa2733bb6399e8c161a819e77b48751a
574515 <574515c0d1ng@gmail.com>
2022-03-17 12:07:47 +0100

    Changed URL for PostGame, added GetGames view

--- server/urls.py
+++ server/urls.py
@@ -8,7 +8,7 @@ from .views import (
 app_name = 'games'
 
 urlpatterns = [
-	path(f'{app_name}/post_game', PostGame.as_view(), name='post_game'),
+	path(f'{app_name}/post_json_game', PostGame.as_view(), name='post_game'),
     path(f'{app_name}/get_games', GetGames, name='get_all_games'),
     path(f'{app_name}/get_games/<int:game_id>', GetGames, name='get_game_by_id'),
     path(f'{app_name}/get_games/<str:user>', GetGames, name='get_games_by_user'),
