ea93a49cbe51cffddc1e743faab1c03cca710eb8
574515 <574515c0d1ng@gmail.com>
2022-03-17 13:03:07 +0100

    Beautified, optimized

--- server/models.py
+++ server/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Game(models.Model):
-	player1                     = models.CharField(max_length=30)
-	player2                     = models.CharField(max_length=30)
-	winner                      = models.CharField(max_length=30)
+	player1						= models.CharField(max_length=30, blank=False, null=False)
+	player2						= models.CharField(max_length=30, blank=False, null=False)
+	winner						= models.CharField(max_length=30, blank=False, null=False)
 
 	def __str__(self):
 		return f'{self.player1} vs {self.player2} -> {self.winner}'
 
 
 class Move(models.Model):
-	move_number                 = models.IntegerField()
-	affected_field              = models.IntegerField()
+	move_number					= models.IntegerField()
+	affected_field				= models.IntegerField()
 	game						= models.ForeignKey(Game, related_name='moves', on_delete=models.CASCADE, blank=False, null=False)
 
 	def __str__(self):
